1db8306136a5694ff34efcefdf9cc035717e9d23
itirsina98 <itirsina98@gmail.com>
2023-03-05 21:24:25 +0200

    Audit base entity of db context save changessdasda

--- Battleship.Persistence/ApplicationDbContext.cs
+++ Battleship.Persistence/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Battleship.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Battleship.Persistence.EntityDbConfigurations;
+using Battleship.Domain.Common;
 
 namespace Battleship.Persistence
 {
@@ -18,6 +19,39 @@ namespace Battleship.Persistence
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserEntityDbConfiguration).Assembly);
             base.OnModelCreating(modelBuilder);
+        }
+
+        // TODO : soft delete and hard delete
+        private void AuditBaseEntities()
+        {
+            var entries = ChangeTracker.Entries().Where((e) => e.Entity is BaseEntity && (e.State == EntityState.Modified || e.State == EntityState.Added));
+
+            foreach(var entry in entries)
+            {
+                var now = DateTime.UtcNow;
+
+                if(entry.State == EntityState.Added)
+                {
+                    ((BaseEntity)entry.Entity).DateCreated = now;
+                }
+
+                if(entry.State == EntityState.Modified)
+                {
+                    ((BaseEntity)entry.Entity).DateUpdated = now;
+                }
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            AuditBaseEntities();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            AuditBaseEntities();
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
